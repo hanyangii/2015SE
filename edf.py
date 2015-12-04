7c0466cb530bc695efb198cdda9f03eef57ea5b5
yuni <id9595@naver.com>
2015-12-04 16:23:12 +0900

    edf and usr, gui start

---
+++ edf.py
@@ -0,0 +1,93 @@
+import math
+
+tasks = []
+
+class Task:
+	def __init__(self,cpuId,start, period, executiontime):
+		self.cpuId=cpuId
+		self.start=start
+		self.period = period
+		self.tmp = period
+		self.executiontime=executiontime
+		self.cur=0
+		self.isend=False
+	
+	def __repr__(self):
+		return '<{} {} {} {}>'.format(self.cpuId, self.start, self.period, self.executiontime)
+
+def ispossible(tasks):
+	cpuUse=0
+	for i in tasks:
+		cpuUse = float(i.executiontime)/float(i.period)
+		
+	if cpuUse<1: return True
+	else: return False
+
+def edf(tasks):
+	outputFile=open("output.txt",'w')
+	print "edf scheduling"
+	tasks.sort(key=lambda Task:Task.start)
+	print tasks
+	working=[]
+	time=0
+	i=0
+	curCPU= Task(0,0,0,0)
+	startTask=0
+	if ispossible(tasks) == False :
+		print 'it is impossible scheduling'
+		return 
+
+	while True:
+		print time
+
+		while i<len(tasks) and int(tasks[i].start) == int(time):
+			working.append(tasks[i])
+			startTask+=1
+			i+=1
+		
+		for x in working :
+			if x.isend==True and x.period==time:
+				x.isend=False
+				x.cur=0
+				x.period += x.tmp
+				print 'it is' + str(x) 
+
+		if len(working)>0:
+			if curCPU.cur == curCPU.executiontime:
+				curCPU.isend=True
+
+		if len(working)>0:
+			working.sort(key=lambda Task:Task.period)
+			for j in working:
+				if j.isend==False:
+					curCPU=j
+					break
+			curCPU.cur+=1
+
+			print str(curCPU) + ' ' + str(curCPU.cpuId)
+			outputFile.write(str(curCPU)+'\n')
+		
+		time+=1
+		if time > 200: break
+
+
+
+def edf_scheduling(task_num):
+	print "edfedf"
+#	filename = input("please write filename: ")
+#	filename = str(filename)+'.txt'
+	filename = 'input.txt'
+	inputFile=open(filename,'w')
+
+	for i in range(task_num):
+		start = input("start %d: " %i)
+		period = input("period %d :" %i)
+		executiontime = input("execution time %d: " %i)
+		
+		tasks.append(Task(i,start,period, executiontime))
+		start = str(i)+" "+str(start)+" "+str(period)+" "+str(executiontime)+" "+"\n"
+		inputFile.write(str(start))
+	inputFile.close()
+	edf(tasks)
+
+
---
+++ usr.py
@@ -0,0 +1,89 @@
+import math
+
+tasks = []
+
+class Task:
+	def __init__(self,cpuId,start,period, executiontime, priority):
+		self.cpuId=cpuId
+		self.start=start
+		self.period=period
+		self.deadline=period
+		self.executiontime=executiontime
+		self.priority=priority
+		self.isend=False
+		self.cur=0
+	
+	def __repr__(self):
+		return '<{} {} {} {}>'.format(self.cpuId, self.start, self.period, self.priority)
+
+def ispossible(tasks):
+	return True
+
+def user(tasks):
+	print "usr scheduling"
+	tasks.sort(key=lambda Task:Task.start)
+	print tasks
+	working=[]
+	time=0
+	i=0
+	curCPU=0
+	startTask=0
+	if ispossible(tasks) == False :
+		print 'it is impossible scheduling'
+		return 
+
+	while True:
+		#print tasks[i] 
+		print time
+		#print tasks[i].start
+		
+		if len(working)>0:
+			if curCPU.executiontime == curCPU.cur:
+				curCPU.isend=True
+				curCPU.cur=0
+		
+		while i<len(tasks) and int(tasks[i].start) == int(time):
+			working.append(tasks[i])
+			startTask+=1
+			i+=1
+		
+		if len(working)>0:
+			for j in working:
+				if j.deadline== time:
+					j.deadline+=j.period
+					j.isend=False
+					
+			working.sort(key=lambda Task:Task.priority)
+			
+			for k in working:
+				if k.isend==False:
+					curCPU=k
+					break
+
+			print str(curCPU) + ' ' + str(curCPU.cpuId)
+		
+		time+=1
+		if time > 20: break
+
+
+
+def usr_scheduling(task_num):
+	print "usrusr"
+#	filename = input("please write filename: ")
+#	filename = str(filename)+'.txt'
+	filename = 'input.txt'
+	inputFile=open(filename,'w')
+
+	for i in range(task_num):
+		start = input("start %d: " %i)
+		period = input("period %d :" %i)
+		executiontime = input("execution time %d: " %i)
+		priority = input("priority %d: " %i)
+		
+		tasks.append(Task(i,start,period, executiontime, priority))
+		start = str(i)+" "+str(start)+" "+str(period)+" "+str(executiontime)+" "+str(priority)+"\n"
+		inputFile.write(str(start))
+	inputFile.close()
+	user(tasks)
+
+
